b1132da7ccff8ecb85567b4c63294cfe7e48a3e9
AtiwadkarAjinkya <ajinkya.atiwadkar@gmail.com>
2021-11-26 08:43:19 +0530

    Second Terraform commit

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -11,18 +11,51 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "image_id" {
+  type = string
+  default = "ami-083654bd07b5da81d"
+  description = "This is AMI ID"
+}
+
+
+resource "aws_key_pair" "IaC" {
+  key_name   = "IaC"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-083654bd07b5da81d"
+  ami           = var.image_id
+  key_name      = "IaC"
   instance_type = "t2.micro"
+  security_groups = ["allow_ssh"]
   tags = {
-    Name = "HelloWorld"
+    Name = "CreatedByIaC"
   }
 }
 
-resource "aws_instance" "web_1" {
-  ami           = "ami-083654bd07b5da81d"
-  instance_type = "t2.micro"
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = "vpc-b266edcf"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
-    Name = "Webserver"
+    Name = "allow_ssh"
   }
 }
