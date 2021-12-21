4412448f0d4714845261174d4919a26ea072077e
AtiwadkarAjinkya <ajinkya.atiwadkar@gmail.com>
2021-12-21 08:04:52 +0530

    Updated main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,11 +2,30 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.69.0"
+      version = "3.70.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "terraform-key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}
+
+
+
+resource "aws_instance" "Demo_ec2" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0b0c0246"
+  key_name = aws_key_pair.deployer.key_name
+
+  tags = {
+    Name = "Test_ec2"
+    CreatedBy = "Terraform"
+  }
 }
