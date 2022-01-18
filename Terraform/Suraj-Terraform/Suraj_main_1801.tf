cbd16b3af696004415b340b3a08b9069562f1d93
suraj-2142 <melshankares@gmail.com>
2022-01-18 14:21:54 +0530

    terraform key from machine

--- Terraform/Suraj-Terraform/Suraj_main_1801.tf
+++ Terraform/Suraj-Terraform/Suraj_main_1801.tf
@@ -12,6 +12,11 @@ provider "aws" {
  
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "Terrafor-Key-suraj"
+  public_key = file('.\id_rsa.pub')
+}
+
 resource "aws_instance" "Terra-Demo" {
   ami           = "ami-001089eb624938d9f"
   instance_type = "t2.micro"
