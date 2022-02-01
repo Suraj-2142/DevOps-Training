e00c56607208dc43465f12ff996a5dae04ee34ce
suraj-2142 <melshankares@gmail.com>
2022-02-01 17:18:22 +0530

    Git push 1 feb

--- Terraform/Suraj-Terraform/Suraj_main_Jan22.tf
+++ Terraform/Suraj-Terraform/Suraj_main_Jan22.tf
@@ -24,7 +24,7 @@ resource "aws_instance" "Terra-Demo" {
   ami           = "ami-001089eb624938d9f"
   instance_type = "t2.micro"
   subnet_id = "subnet-01a956015fff8a630"
-  key_name = "SURAJ"
+  key_name = "VPC"
  // key_name = "aws_key_pair.deployer.key_name"
     
 
