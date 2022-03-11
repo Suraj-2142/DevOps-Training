39434709e0a88c50782ab727d1152db6ee0fc5c8
suraj-2142 <melshankares@gmail.com>
2022-03-11 12:19:40 +0530

    Code issue

---
+++ Terraform/Suraj-Terraform/terraform.tfvars
@@ -0,0 +1,1 @@
+image_ids = "ami-0fb653ca2d3203ac1"
--- Terraform/Suraj-Terraform/var.tf
+++ Terraform/Suraj-Terraform/var.tf
@@ -1,4 +1,4 @@
-variable "image_id" {
+variable "image_ids" {
   type = string
   default = "ami-0fb653ca2d3203ac1"
   description = "ID of machine imgae AMI"
